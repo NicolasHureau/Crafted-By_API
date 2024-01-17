e5d345e4cc0a91322af3e4267569bfbdccfee90a
Nicolas <nicolas.hureau@le-campus-numerique.fr>
2024-01-17 14:53:42 +0100

    update id to uuid in user, business and tokens models https://dev.to/adnanbabakan/implement-uuid-primary-key-in-laravel-and-its-benefits-55o3

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Business extends Model
 {
     use HasFactory;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+//
+
+    public static function booted(): void
+    {
+        static::creating(function($model) {
+            $model->id = Str::uuid();
+        });
+    }
+
 }
