186ebf2402790ad7108168f7401d8b50f5fe79c9
Nicolas <nicolas.hureau@le-campus-numerique.fr>
2024-01-17 09:56:06 +0100

    complete user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('lastname');
+            $table->string('firstname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address');
+            $table->integer('zip_code_id');
+            $table->integer('city_id');
             $table->rememberToken();
             $table->timestamps();
         });
